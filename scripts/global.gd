bed518eb7ba0f3c29f0b2af45ef6c4a706cca7eb
“Echo” <echorw927@gmail.com>
2024-09-14 16:21:04 -0700

    flag works mostly

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,1 +1,25 @@
 extends Node
+
+
+
+# Level Management
+var level = 1
+var chapter = 1
+
+
+
+func nextlevel():
+	var temppath = "res://scenes/chapters/%s/level_%s.tscn"
+	var pathname = temppath % [chapter, level+1]
+	if ResourceLoader.exists(pathname):
+		level += 1
+		get_tree().change_scene_to_file(pathname)
+	else:
+		get_tree().change_scene_to_file(get_tree().current_scene.scene_file_path)
+
+
+
+
+var ch1 = [
+	"res://scenes/chapters/1/level_1.tscn",
+]
